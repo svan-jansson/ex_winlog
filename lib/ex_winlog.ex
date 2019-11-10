fc578b04f33a2406ee5e965ab991c35db85306b4
Erik Svensson <erik.svensson@asolvi.com>
2019-11-10 13:35:38 +0100

    chore: removing boilerplate code

--- lib/ex_winlog.ex
+++ lib/ex_winlog.ex
@@ -1,18 +1,3 @@
 defmodule ExWinlog do
-  @moduledoc """
-  Documentation for ExWinlog.
-  """
 
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> ExWinlog.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
--- test/ex_winlog_test.exs
+++ test/ex_winlog_test.exs
@@ -2,7 +2,4 @@ defmodule ExWinlogTest do
   use ExUnit.Case
   doctest ExWinlog
 
-  test "greets the world" do
-    assert ExWinlog.hello() == :world
-  end
 end
