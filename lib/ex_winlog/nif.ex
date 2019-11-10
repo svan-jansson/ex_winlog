2448c1fbb3fce007654537fe8c60d22a844b14f0
Erik Svensson <erik.svensson@asolvi.com>
2019-11-10 15:29:34 +0100

    feat: added possibility to deregister event sources

--- lib/ex_winlog/nif.ex
+++ lib/ex_winlog/nif.ex
@@ -3,4 +3,5 @@ defmodule ExWinlog.Nif do
     use Rustler, otp_app: :ex_winlog, crate: "ex_winlog_nif"
 
     def register(_event_source_name), do: :erlang.nif_error(:nif_not_loaded)
+    def deregister(_event_source_name), do: :erlang.nif_error(:nif_not_loaded)
 end
--- native/ex_winlog_nif/src/lib.rs
+++ native/ex_winlog_nif/src/lib.rs
@@ -6,7 +6,8 @@ mod atoms {
     rustler_atoms! {
         atom ok;
         atom error;
-        atom event_source_created;
+        atom event_source_registered;
+        atom event_source_deregistered;
         atom insufficient_permissions;
         atom io_error;
         atom exe_path_not_found;
@@ -16,7 +17,8 @@ mod atoms {
 rustler::rustler_export_nifs! {
     "Elixir.ExWinlog.Nif",
     [
-        ("register", 1, register)
+        ("register", 1, register),
+        ("deregister", 1, deregister)
     ],
     None
 }
@@ -24,7 +26,17 @@ rustler::rustler_export_nifs! {
 fn register<'a>(env: Env<'a>, args: &[Term<'a>]) -> Result<Term<'a>, Error> {
     let source: &str = args[0].decode()?;
     match winlog::try_register(&source) {
-        Ok(_result) => Ok((atoms::ok(), atoms::event_source_created()).encode(env)),
+        Ok(_result) => Ok((atoms::ok(), atoms::event_source_registered()).encode(env)),
+        Err(winlog::Error::RegisterSourceFailed) => Ok((atoms::error(), atoms::insufficient_permissions()).encode(env)),
+        Err(winlog::Error::Io(reason)) => Ok((atoms::error(), atoms::io_error(), reason.to_string()).encode(env)),
+        Err(winlog::Error::ExePathNotFound) => Ok((atoms::error(), atoms::exe_path_not_found()).encode(env))
+    }
+}
+
+fn deregister<'a>(env: Env<'a>, args: &[Term<'a>]) -> Result<Term<'a>, Error> {
+    let source: &str = args[0].decode()?;
+    match winlog::try_deregister(&source) {
+        Ok(_result) => Ok((atoms::ok(), atoms::event_source_deregistered()).encode(env)),
         Err(winlog::Error::RegisterSourceFailed) => Ok((atoms::error(), atoms::insufficient_permissions()).encode(env)),
         Err(winlog::Error::Io(reason)) => Ok((atoms::error(), atoms::io_error(), reason.to_string()).encode(env)),
         Err(winlog::Error::ExePathNotFound) => Ok((atoms::error(), atoms::exe_path_not_found()).encode(env))
