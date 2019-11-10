2332c2146aade79e3b0f08dedcc2d4c69b598f3d
Erik Svensson <erik.svensson@asolvi.com>
2019-11-10 13:24:28 +0100

    init: mix new ex_winlog

---
+++ lib/ex_winlog.ex
@@ -0,0 +1,18 @@
+defmodule ExWinlog do
+  @moduledoc """
+  Documentation for ExWinlog.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ExWinlog.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/ex_winlog_test.exs
@@ -0,0 +1,8 @@
+defmodule ExWinlogTest do
+  use ExUnit.Case
+  doctest ExWinlog
+
+  test "greets the world" do
+    assert ExWinlog.hello() == :world
+  end
+end
