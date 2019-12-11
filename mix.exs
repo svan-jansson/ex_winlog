61b77bd2319de8a1d7710a45848ceee95d986a43
Svan Jansson <svan.jansson@gmail.com>
2019-12-11 15:18:18 +0100

    chore: increased version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule ExWinlog.MixProject do
   def project do
     [
       app: :ex_winlog,
-      version: "0.1.3",
+      version: "0.1.4",
       elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
